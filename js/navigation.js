512313ec262293ee8a0f336f476de5d93dff1587
TusharSachde <tushar@wohlig.com>
2016-01-14 19:58:14 +0530

    Done

--- js/navigation.js
+++ js/navigation.js
@@ -5,51 +5,61 @@ var navigationservice = angular.module('navigationservice', [])
     name: "Happyness Quotient",
     classis: "active",
     anchor: "happyness",
+    target: "_self",
     subnav: []
   }, {
     name: "Watch Video",
     classis: "active",
     anchor: "happinessquotient",
+    target: "_self",
     subnav: []
   }, {
     name: "What's In Store",
     classis: "active",
     anchor: "packages",
+    target: "_self",
     subnav: []
   }, {
     name: "How is Happyness Quotient Different?",
     classis: "active",
     anchor: "hqisunique",
+    target: "_self",
     subnav: []
   }, {
     name: "What's next?",
     classis: "active",
     anchor: "services",
+    target: "_self",
     subnav: []
   }, {
     name: "Let's Talk",
     classis: "active",
     anchor: "contact",
+    target: "_self",
     subnav: []
   }, {
     name: "Visit Never Grow Up®",
     classis: "active",
     link: "http://www.willnevergrowup.com/",
+    target: "_blank",
     subnav: []
   }, {
     name: "Visit Our Blog",
     classis: "active",
     link: "http://www.willnevergrowup.com/blog/",
+    target: "_blank",
     subnav: []
   }, {
     name: "About Us",
     classis: "active",
     link: "http://www.willnevergrowup.com/about.html",
+    target: "_blank",
     subnav: []
   }, {
-    name: "Store",
+    name: "Packages",
     classis: "active",
     link: "#/storedetail",
+    target: "_self",
     subnav: []
   }];
 
